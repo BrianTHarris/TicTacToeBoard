9cfeeacf21b87fc70885c674897eb1836a02ed42
brian <the6harris@gmail.com>
2019-11-20 19:18:00 -0800

    finished getwinner want push computer slow

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -79,5 +79,36 @@ Piece TicTacToeBoard::getPiece(int row, int column)
  **/
 Piece TicTacToeBoard::getWinner()
 {
-	return Invalid;
+	for(i=0; i<3; ++i)
+	{
+		for(j=0; j<3, ++j)
+		{
+			if(board[i][j] == Blank)
+				return Invalid;
+		}
+	}
+	// if get here must mean board is filled
+	if( board[0][0] == board[1][1] && board[2][2] == board[0][0])
+	{
+		return board[0][0];
+	}
+	else if( board[2][0] == board[1][1] && board[0][2] == board[0][0])
+	{
+		return board[1][1];
+	}
+
+	for(i=0; i<3; ++i)
+	{
+
+		if( board[i][0] == board[i][1] && board[i][2] == board[i][0])
+			return board[i][0];
+	}
+
+	for(i=0; i<3; ++i)
+	{
+
+		if( board[0][i] == board[1][i] && board[2][i] == board[0][i])
+			return board[0][i];
+	}
+
 }
