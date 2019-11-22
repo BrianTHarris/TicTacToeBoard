c4ecdc2a87decd68e7e950de538a667fd4aef771
brian <the6harris@gmail.com>
2019-11-22 12:13:21 -0800

    all tests for getWinner

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -130,3 +130,53 @@ ex.placePiece(2,2);//o
 ex.placePiece(0,2);//x
 ASSERT_EQ(X,ex.getWinner());
 }
+
+TEST(TicTacToeBoardTest, getwinnersidetoside)
+{
+TicTacToeBoard ex;
+ex.placePiece(0,0);//x
+ex.placePiece(1,1);//o
+ex.placePiece(0,0);//x
+ex.placePiece(1,0);//o
+ex.placePiece(2,2);//x
+ex.placePiece(1,2);//o
+ASSERT_EQ(O,ex.getWinner());
+}
+
+TEST(TicTacToeBoardTest, getwinnersvert)
+{
+TicTacToeBoard ex;
+ex.placePiece(0,0);//x
+ex.placePiece(1,1);//o
+ex.placePiece(0,0);//x
+ex.placePiece(0,1);//o
+ex.placePiece(2,2);//x
+ex.placePiece(2,1);//o
+ASSERT_EQ(O,ex.getWinner());
+}
+
+TEST(TicTacToeBoardTest, getwinnerblankspot)
+{
+TicTacToeBoard ex;
+Piece temp=ex.placePiece(0,1);
+Piece temp=ex.placePiece(0,2);
+Piece temp=ex.placePiece(2,2);
+Piece temp=ex.placePiece(2,1);
+Piece temp=ex.placePiece(1,1);
+ASSERT_EQ(Invalid, ex.getWinner());
+}
+
+TEST(TicTacToeBoardTest, tiegame)
+{
+TicTacToeBoard ex;
+Piece temp=ex.placePiece(0,1);
+Piece temp=ex.placePiece(0,2);
+Piece temp=ex.placePiece(2,2);
+Piece temp=ex.placePiece(2,1);
+Piece temp=ex.placePiece(1,1);
+Piece temp=ex.placePiece(1,2);//o
+Piece temp=ex.placePiece(1,0);//x
+Piece temp=ex.placePiece(0,0);//o
+Piece temp=ex.placePiece(2,0);//x
+ASSERT_EQ(Blank, ex.getWinner());
+}
