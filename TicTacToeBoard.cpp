9f36b0b7357de0cfff03cb5e2163fd78e11458bf
brian <the6harris@gmail.com>
2019-11-22 09:46:28 -0800

    trying to get diagnolup to work

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -84,7 +84,7 @@ Piece TicTacToeBoard::getWinner()
 	{
 		return board[0][0];
 	}
-	else if( board[2][0] == board[1][1] && board[0][2] == board[0][0])
+	if( board[2][0] == board[1][1] && board[0][2] == board[0][0])
 	{
 		return board[1][1];
 	}
