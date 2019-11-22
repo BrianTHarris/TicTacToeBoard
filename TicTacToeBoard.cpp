874dd5d120316e48705e150aede8e18a2558a7cb
brian <the6harris@gmail.com>
2019-11-22 12:54:17 -0800

    if not blank

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -80,11 +80,11 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 Piece TicTacToeBoard::getWinner()
 {
 
-	if( board[0][0] == board[1][1] && board[2][2] == board[0][0])
+	if( board[0][0] == board[1][1] && board[2][2] == board[0][0] && board[0][0] != Blank)
 	{
 		return board[0][0];
 	}
-	if( board[2][0] == board[1][1] && board[0][2] == board[1][1])
+	if( board[2][0] == board[1][1] && board[0][2] == board[1][1] && board[1][1] != Blank)
 	{
 		return board[1][1];
 	}
@@ -92,14 +92,14 @@ Piece TicTacToeBoard::getWinner()
 	for(int i=0; i<3; ++i)
 	{
 
-		if( board[i][0] == board[i][1] && board[i][2] == board[i][0])
+		if( board[i][0] == board[i][1] && board[i][2] == board[i][0] && board[i][0] != Blank)
 			return board[i][0];
 	}
 
 	for(int i=0; i<3; ++i)
 	{
 
-		if( board[0][i] == board[1][i] && board[2][i] == board[0][i])
+		if( board[0][i] == board[1][i] && board[2][i] == board[0][i] && board[0][i] != Blank)
 			return board[0][i];
 	}
 
@@ -111,8 +111,5 @@ Piece TicTacToeBoard::getWinner()
 				return Invalid;
 		}
 	}
-	if(board[0][0] != Blank && board[0][1] != Blank && board[0][2] != Blank && board[1][0] != Blank && board[1][1] != Blank && board[1][2] != Blank && board[2][0] != Blank && board[2][1] != Blank && board[2][1] != Blank) 
 		return Blank;
-	else
-		return Invalid;
 }
